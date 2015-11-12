efbcaa76863ab9ac29b2bea6ea70688294bb3460
BuriedJet <jetgabr@gmail.com>
2015-11-12 21:28:59 +0800

    Trying. First commit.

---
+++ Othello/pltfmbase.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//cross-platform implement
+#define PAUSE getchar();getchar();
+
+#ifdef _WIN32
+#include <windows.h>
+#define CLS system("cls")
+#define SLP(X) Sleep(X)
+#endif
+
+
+#ifdef linux
+#include <system.h>
+#define CLS system("clear")
+#define SLP(X) usleep(X)
+#endif
+
+
+//GLUT flaw fix
+#ifndef GLUT_DISABLE_ATEXIT_HACK
+#define GLUT_DISABLE_ATEXIT_HACK
+#endif
